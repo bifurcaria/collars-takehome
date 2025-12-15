02860513af7695329adf5dcf69f794f18c19c383
carla <106553998+bifurcaria@users.noreply.github.com>
2025-12-15 13:22:38 -0300

    chore(layout): address header title issue in MoviesLayout with ScrollView in Expo

--- app/(tabs)/index/_layout.tsx
+++ app/(tabs)/index/_layout.tsx
@@ -8,6 +8,10 @@ export default function MoviesLayout() {
   return (
     <Stack screenOptions={{ 
       headerLargeTitle: true, 
+      // (Large header title disappears when using ScrollView as content in Expo Native Tab screen)
+      // This is an issue related to Apple's UIKit and not related to this Repository, as this can be reproduced without react-native-screens, on which Expo builds on.
+      // See the related issue in https://github.com/expo/expo/issues/40717.
+      // The current solution to this is to set headerTransparent: true and headerBlurEffect: "regular" on the Stack Screen options.
       headerTransparent: Platform.select({ ios: true, default: false }),
       headerTitleStyle: {
         fontFamily: Platform.select({ ios: 'System', android: Typography.title, default: Typography.title }),
